7ef11474103f34176746dc6b3c3fc1ead68478b1
imloadinqqq <107895308+imloadinqqq@users.noreply.github.com>
2024-07-14 17:39:03 -0500

    end of july commit

---
+++ june-webpage/src/resume/resume.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function resume() {
+
+    return (
+        <div>
+            <h1>This is where I would put my resume at.</h1>
+        </div>
+    )
+}
+
+export default resume
